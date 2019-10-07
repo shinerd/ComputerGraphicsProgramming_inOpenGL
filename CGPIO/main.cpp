96ed87b44de1a5424488f99a02f0de79681446f8
Shinernd <vnflqkq@gmail.com>
2019-10-07 17:06:40 -0400

    Program 2.1 First C++/OpenGL Application

--- CGPIO/main.cpp
+++ CGPIO/main.cpp
@@ -10,23 +10,43 @@ void init (GLFWwindow* window) {}
 
 void display(GLFWwindow* window, double currentTime) {
     glClearColor(1.0, 0.0, 0.0, 1.0);
+    
+    // void glClear(GLbitfield mask)
+    // GL_COLOR_BUFFERE_BIT : references the color buffer
+    // glClear() : clears all of color buffers that OpenGL has
     glClear(GL_COLOR_BUFFER_BIT);
 }
 
 int main(void) {
     if (!glfwInit()) {exit(EXIT_FAILURE);}
+    
+    // OpenGL version 2.0
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    
+    // monitor, share: for full screen moce and resource sharing
     GLFWwindow* window = glfwCreateWindow(600, 600, "Chapter2 - program1", nullptr, nullptr);
     glfwMakeContextCurrent(window);
     if (glewInit() != GLEW_OK) {exit(EXIT_FAILURE);}
+    
+    // vertical synchronization (VSyc)
     glfwSwapInterval(1);
     
+    // includes a reference to the GLFW window object
     init(window);
     
+    // rendering loop
+    // glfwWindowShouldClose : makes the loop terminate when GLFW detects an sould-close event
     while (!glfwWindowShouldClose(window)) {
+        
+        // includes a reference to the GLFW windows object and the current time
         display(window, glfwGetTime());
+        
+        // vertical synchronization (VSyc)
+        // paints the screen
         glfwSwapBuffers(window);
+        
+        // handles other window-related events
         glfwPollEvents();
     }
     
