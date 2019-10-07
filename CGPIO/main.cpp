1e0ceee39cbc4c2c93e60eb9e1e6ccb2c53e76d2
Shinernd <vnflqkq@gmail.com>
2019-10-06 21:15:08 -0400

    Initial Commit

---
+++ CGPIO/main.cpp
@@ -0,0 +1,36 @@
+//  Copyright © 2019 Shinernd. All rights reserved.
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+using namespace std;
+
+void init (GLFWwindow* window) {}
+
+void display(GLFWwindow* window, double currentTime) {
+    glClearColor(1.0, 0.0, 0.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
+int main(void) {
+    if (!glfwInit()) {exit(EXIT_FAILURE);}
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    GLFWwindow* window = glfwCreateWindow(600, 600, "Chapter2 - program1", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+    if (glewInit() != GLEW_OK) {exit(EXIT_FAILURE);}
+    glfwSwapInterval(1);
+    
+    init(window);
+    
+    while (!glfwWindowShouldClose(window)) {
+        display(window, glfwGetTime());
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+}
