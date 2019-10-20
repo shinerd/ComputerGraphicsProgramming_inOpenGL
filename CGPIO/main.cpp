9f0659c1ae739333fdef46ab27638a6f9bc3d70f
shinerd <vnflqkq@gmail.com>
2019-10-19 22:44:33 -0400

    Exercises 2.1 Grow and Shrink the Point Size

--- CGPIO/main.cpp
+++ CGPIO/main.cpp
@@ -2,115 +2,44 @@
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include <string>
 #include <iostream>
-#include <fstream>
 
 #define numVAOs 1
 
 GLuint renderingProgram;
 GLuint vao[numVAOs];
 
-// location of triangle on x axis
 float x = 0.0f;
-// offset for moving the triangle
-float inc = 0.01f;
+float inc = 1.0f;
+float size[5];
 
 using namespace std;
 
-void printShaderLog(GLuint shader) {
-    int len = 0;
-    int chWrittn = 0;
-    char* log;
-    glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &len);
-    if (len > 0) {
-        log = (char*)malloc(len);
-        glGetShaderInfoLog(shader, len, &chWrittn, log);
-        cout << "Shader Info Log: " << log << endl;
-        free(log);
-    }
-}
-
-void printProgramLog(int prog) {
-    int len = 0;
-    int chWrittn = 0;
-    char* log;
-    glGetProgramiv(prog, GL_INFO_LOG_LENGTH, &len);
-    if (len > 0) {
-        log = (char*)malloc(len);
-        glGetProgramInfoLog(prog, len, &chWrittn, log);
-        cout << "Program Info Log: " << log << endl;
-        free(log);
-    }
-}
-
-bool checkOpenGLError() {
-    bool foundError = false;
-    int glErr = glGetError();
-    while (glErr != GL_NO_ERROR) {
-        cout << "glError: " << glErr << endl;
-        foundError = true;
-        glErr = glGetError();
-    }
-    return foundError;
-}
-
-string readShaderSource(const char *filePath) {
-    string content = "";
-    ifstream fileStream(filePath, ios::in);
-    string line = "";
-    while (!fileStream.eof()) {
-        getline(fileStream, line);
-        content.append(line + "\n");
-    }
-    fileStream.close();
-    return content;
-}
-
 GLuint createShaderProgram() {
-    GLint vertCompiled;
-    GLint fragCompiled;
-    GLint linked;
-    
-    string vertShaderStr = readShaderSource("/Users/shinerd/Documents/ComputerGraphicsProgramming_inOpenGL/CGPIO/vertShader.glsl");
-    string fragShaderStr = readShaderSource("/Users/shinerd/Documents/ComputerGraphicsProgramming_inOpenGL/CGPIO/fragShader.glsl");
-    
-    const char* vertShaderSrc = vertShaderStr.c_str();
-    const char* fragShaderSrc = fragShaderStr.c_str();
+    const char* vshaderSource =
+    "#version 410 \n"
+    "void main(void) \n"
+    "{gl_Position = vec4(0.0, 0.0, 0.0, 1.0);}";
+    const char* fshaderSource =
+    "#version 410 \n"
+    "out vec4 color; \n"
+    "void main(void) \n"
+    "{color = vec4(0.0, 0.0, 1.0, 1.0);}";
     
     GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
     GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
     
-    glShaderSource(vShader, 1, &vertShaderSrc, nullptr);
-    glShaderSource(fShader, 1, &fragShaderSrc, nullptr);
-    
+    glShaderSource(vShader, 1, &vshaderSource, nullptr);
+    glShaderSource(fShader, 1, &fshaderSource, nullptr);
     glCompileShader(vShader);
-    checkOpenGLError();
-    glGetShaderiv(vShader, GL_COMPILE_STATUS, &vertCompiled);
-    if (vertCompiled != 1) {
-        cout << "vertex compilation failed" << endl;
-        printShaderLog(vShader);
-    }
-    
     glCompileShader(fShader);
-    checkOpenGLError();
-    glGetShaderiv(fShader, GL_COMPILE_STATUS, &fragCompiled);
-    if (fragCompiled != 1) {
-        cout << "fragment compilation failed" << endl;
-        printShaderLog(fShader);
-    }
     
     GLuint vfProgram = glCreateProgram();
+
     glAttachShader(vfProgram, vShader);
     glAttachShader(vfProgram, fShader);
     
     glLinkProgram(vfProgram);
-    checkOpenGLError();
-    glGetProgramiv(vfProgram, GL_LINK_STATUS, &linked);
-    if (linked != 1) {
-        cout << "linking failed" << endl;
-        printProgramLog(vfProgram);
-    }
     
     return vfProgram;
 }
@@ -123,27 +52,20 @@ void init (GLFWwindow* window) {
 }
 
 void display(GLFWwindow* window, double currentTime) {
+    glUseProgram(renderingProgram);
     
-    // clear the background to black, each time
+    // Don't forget to clear!!
     glClear(GL_DEPTH_BUFFER_BIT);
     glClearColor(0.0, 0.0, 0.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT);
     
-    glUseProgram(renderingProgram);
-    
-    // move the triangle along x axis
     x += inc;
-    // switch to moving the triangle to the left
-    if (x > 1.0f) inc = -0.01f;
-    // switch to moving the triangle to the right
-    if (x < -1.0f) inc = 0.01f;
+    if (x == 100.0f) inc = -1.0f;
+    if (x == 0.0f) inc = 1.0f;
+    cout << x << endl;
     
-    // get ptr to "offset"
-    GLuint offsetLoc = glGetUniformLocation(renderingProgram, "offset");
-    
-    glProgramUniform1f(renderingProgram, offsetLoc, x);
-    
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glPointSize(x);
+    glDrawArrays(GL_POINTS, 0, 1);
 }
 
 int main(void) {
@@ -154,7 +76,7 @@ int main(void) {
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // and neither this
     GLFWwindow* window = glfwCreateWindow(600, 600, "Chapter2 - program2", nullptr, nullptr);
     glfwMakeContextCurrent(window);
-    if (glewInit() != GLEW_OK) {exit(EXIT_FAILURE);}
+    if (glewInit() != GLEW_OK) {exit(EXIT_FAILURE);}                // without the line 80 or 81, this makes an error
     glfwSwapInterval(1);
     
     init(window);
--- CGPIO/vertShader.glsl
+++ CGPIO/vertShader.glsl
@@ -1,7 +1,3 @@
 #version 410
-uniform float offset;
-void main(void) {
-    if (gl_VertexID == 0) gl_Position = vec4(0.25 + offset, -0.25, 0.0, 1.0);
-    else if (gl_VertexID == 1) gl_Position = vec4(-0.25 + offset, -0.25, 0.0, 1.0);
-    else gl_Position = vec4(0.25 + offset, 0.25, 0.0, 1.0);
-}
+void main(void)
+{gl_Position = vec4(0.0, 0.0, 0.0, 1.0);}
