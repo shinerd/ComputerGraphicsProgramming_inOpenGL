5ca1aba9f58b31b4931536746ff8594d1a8c24e6
shinerd <vnflqkq@gmail.com>
2019-10-23 15:11:54 -0400

    Exercise 3.1 Rotate the Triangle

--- CGPIO/main.cpp
+++ CGPIO/main.cpp
@@ -18,28 +18,15 @@ float inc = 0.01f;
 using namespace std;
 
 void init (GLFWwindow* window) {
-    
-    // Utils
     renderingProgram = Utils::createShaderProgram(
         "/Users/shinerd/Documents/ComputerGraphicsProgramming_inOpenGL/CGPIO/vertShader.glsl",
         "/Users/shinerd/Documents/ComputerGraphicsProgramming_inOpenGL/CGPIO/fragShader.glsl");
-     
     glGenVertexArrays(numVAOs, vao);
     glBindVertexArray(vao[0]);
 }
 
 void display(GLFWwindow* window, double currentTime) {
-    glClear(GL_DEPTH_BUFFER_BIT);
-    glClearColor(0.0, 0.0, 0.0, 1.0);
-    glClear(GL_COLOR_BUFFER_BIT);
-    
     glUseProgram(renderingProgram);
-
-    x += inc;
-    if (x > 1.0f) inc = -0.01f;
-    if (x < -1.0f) inc = 0.01f;
-    GLuint offsetLoc = glGetUniformLocation(renderingProgram, "offset");
-    glProgramUniform1f(renderingProgram, offsetLoc, x);
     glDrawArrays(GL_TRIANGLES, 0, 3);
 }
 
@@ -49,7 +36,7 @@ int main(void) {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // I don't know what this does
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // and neither this
-    GLFWwindow* window = glfwCreateWindow(600, 600, "Chapter2 - program2", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(600, 600, "Chapter3 - exercise1", nullptr, nullptr);
     glfwMakeContextCurrent(window);
     if (glewInit() != GLEW_OK) {exit(EXIT_FAILURE);}
     glfwSwapInterval(1);
