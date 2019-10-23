9ecb5adc6cfb62b3235e897468c02f71a404b172
shinerd <vnflqkq@gmail.com>
2019-10-22 22:46:45 -0400

    Program2.6 with Utils

--- CGPIO/main.cpp
+++ CGPIO/main.cpp
@@ -5,130 +5,39 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include "Utils.h"
 
 #define numVAOs 1
 
 GLuint renderingProgram;
 GLuint vao[numVAOs];
 
+float x = 0.0f;
+float inc = 0.01f;
+
 using namespace std;
 
-// displays the contents of OpenGL's log when GLSL compilation failed
-void printShaderLog(GLuint shader) {
-    int len = 0;
-    int chWrittn = 0;
-    char* log;
-    glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &len);
-    if (len > 0) {
-        log = (char*)malloc(len);
-        glGetShaderInfoLog(shader, len, &chWrittn, log);
-        cout << "Shader Info Log: " << log << endl;
-        free(log);
-    }
-}
-
-// displays the contents of OpenGL's log when GLSL linking failed
-void printProgramLog(int prog) {
-    int len = 0;
-    int chWrittn = 0;
-    char* log;
-    glGetProgramiv(prog, GL_INFO_LOG_LENGTH, &len);
-    if (len > 0) {
-        log = (char*)malloc(len);
-        glGetProgramInfoLog(prog, len, &chWrittn, log);
-        cout << "Program Info Log: " << log << endl;
-        free(log);
-    }
-}
-
-// checks the OpenGL error flag for the occurrrence of an OpenGL error
-// detects both GLSL compilation errors and OpenGL runtime errors
-bool checkOpenGLError() {
-    bool foundError = false;
-    int glErr = glGetError();
-    while (glErr != GL_NO_ERROR) {
-        cout << "glError: " << glErr << endl;
-        foundError = true;
-        glErr = glGetError();
-    }
-    return foundError;
-}
-
-string readShaderSource(const char *filePath) {
-    string content = "";
-    ifstream fileStream(filePath, ios::in);
-    //    cerr << "Error: " << strerror(errno) << endl;  // No such file or directory
-    //    cout << fileStream.is_open() << endl;  // 0
-    string line = "";
-    while (!fileStream.eof()) {
-        getline(fileStream, line);
-        content.append(line + "\n");
-    }
-    fileStream.close();
-    return content;
-}
-
-GLuint createShaderProgram() {
-    GLint vertCompiled;
-    GLint fragCompiled;
-    GLint linked;
-    
-    string vertShaderStr = readShaderSource("/Users/shinerd/Documents/ComputerGraphicsProgramming_inOpenGL/CGPIO/vertShader.glsl");
-    string fragShaderStr = readShaderSource("/Users/shinerd/Documents/ComputerGraphicsProgramming_inOpenGL/CGPIO/fragShader.glsl");
-    
-    const char* vertShaderSrc = vertShaderStr.c_str();
-    const char* fragShaderSrc = fragShaderStr.c_str();
-    
-    GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
-    GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
-    
-    glShaderSource(vShader, 1, &vertShaderSrc, nullptr);
-    glShaderSource(fShader, 1, &fragShaderSrc, nullptr);
-    
-    glCompileShader(vShader);
-    checkOpenGLError();
-    glGetShaderiv(vShader, GL_COMPILE_STATUS, &vertCompiled);
-    if (vertCompiled != 1) {
-        cout << "vertex compilation failed" << endl;
-        printShaderLog(vShader);
-    }
-    
-    glCompileShader(fShader);
-    checkOpenGLError();
-    glGetShaderiv(fShader, GL_COMPILE_STATUS, &fragCompiled);
-    if (fragCompiled != 1) {
-        cout << "fragment compilation failed" << endl;
-        printShaderLog(fShader);
-    }
-    
-    GLuint vfProgram = glCreateProgram();
-    glAttachShader(vfProgram, vShader);
-    glAttachShader(vfProgram, fShader);
-    
-    glLinkProgram(vfProgram);
-    checkOpenGLError();
-    glGetProgramiv(vfProgram, GL_LINK_STATUS, &linked);
-    if (linked != 1) {
-        cout << "linking failed" << endl;
-        printProgramLog(vfProgram);
-    }
-    
-    return vfProgram;
-}
-
-
 void init (GLFWwindow* window) {
-    renderingProgram = createShaderProgram();
+    
+    // Utils
+    renderingProgram = Utils::createShaderProgram(                                                                  "/Users/shinerd/Documents/ComputerGraphicsProgramming_inOpenGL/CGPIO/vertShader.glsl",                      "/Users/shinerd/Documents/ComputerGraphicsProgramming_inOpenGL/CGPIO/fragShader.glsl");
+     
     glGenVertexArrays(numVAOs, vao);
     glBindVertexArray(vao[0]);
 }
 
 void display(GLFWwindow* window, double currentTime) {
-    glUseProgram(renderingProgram);
-    glPointSize(30.0f);
+    glClear(GL_DEPTH_BUFFER_BIT);
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
     
-    // repeat to run "vertShader.glsl" 3 times
-    // gl_VertexID is increased automatically from 0
+    glUseProgram(renderingProgram);
+
+    x += inc;
+    if (x > 1.0f) inc = -0.01f;
+    if (x < -1.0f) inc = 0.01f;
+    GLuint offsetLoc = glGetUniformLocation(renderingProgram, "offset");
+    glProgramUniform1f(renderingProgram, offsetLoc, x);
     glDrawArrays(GL_TRIANGLES, 0, 3);
 }
 
